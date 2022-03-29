2c1ffaf81e22f3d838db9a7b965f5da243fd3f6b
nbenlin <nihatbenli.nb@gmail.com>
2022-03-29 15:16:08 +0200

    Add styling to main component

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,5 +1,19 @@
-import React from "react";
+import classes from "./Navbar.module.css";
+import Logo from "../../images/spacex.svg";
 
 export const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className={classes.navbar}>
+      <div className={classes.logo}>
+        <img src={Logo} alt="spacex" />
+      </div>
+      <ul className={classes.list}>
+        <li className={classes.item}>Home</li>
+        <li className={classes.item}>Launches</li>
+        <li className={classes.item}>Rockets</li>
+        <li className={classes.item}>Missions</li>
+        <li className={classes.item}>Ships</li>
+      </ul>
+    </nav>
+  );
 };
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,12 +5,10 @@ import Home from "../pages/Home";
 const Routing = () => {
   return (
     <Router>
-      <div>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </Router>
   );
 };
