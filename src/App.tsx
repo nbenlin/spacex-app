2762b8c6152d5143d38dddb61897f0dd05cebb4f
nbenlin <nihatbenli.nb@gmail.com>
2022-03-30 10:48:16 +0200

    Add modify to Launches component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
-import { ApolloProvider, InMemoryCache, ApolloClient } from "@apollo/client";
+import {
+  ApolloProvider,
+  InMemoryCache,
+  ApolloClient,
+  gql,
+} from "@apollo/client";
+
+import { offsetLimitPagination } from "@apollo/client/utilities";
 import "./App.css";
 import Routing from "./routes";
 
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        launchesPast: offsetLimitPagination(),
+      },
+    },
+  },
+});
+
 const client = new ApolloClient({
-  uri: "https://api.spacex.land/graphql",
-  cache: new InMemoryCache(),
+  uri: "https://api.spacex.land/graphql/",
+  cache,
 });
 
 const App = () => {
--- src/pages/Launches/Launches.tsx
+++ src/pages/Launches/Launches.tsx
@@ -1,10 +1,16 @@
 import { gql, useQuery } from "@apollo/client";
+
 import LaunchList from "../../components/LaunchesList";
 import Loader from "../../components/Loader";
 
-const GET_LAUNCHES_QUERY = gql`
-  {
-    launchesPast(limit: 15) {
+export const LIST_LAUNCHES = gql`
+  query ListLaunches($offset: Int!, $limit: Int!) {
+    launches: launchesPast(
+      offset: $offset
+      limit: $limit
+      sort: "launch_date_utc"
+      order: "desc"
+    ) {
       mission_name
       launch_site {
         site_name_long
@@ -19,21 +25,30 @@ const GET_LAUNCHES_QUERY = gql`
 `;
 
 export const Launches: React.FC = () => {
-  const { data, loading } = useQuery(GET_LAUNCHES_QUERY);
+  const { data, error, fetchMore, variables, loading } = useQuery(
+    LIST_LAUNCHES,
+    {
+      notifyOnNetworkStatusChange: true,
+      variables: {
+        offset: 0,
+        limit: 10,
+      },
+    }
+  );
 
   if (loading) return <Loader />;
+
+  if (error) return <div>{error.message}</div>;
 
   const launchesData = data.launchesPast.filter(
     (launch: any) =>
       launch.links.article_link && launch.links.flickr_images.length > 0
   );
 
-  console.log("dat", data);
-
   return (
-    <div>
-      <h1>Launchers</h1>
+    <>
+      <h1 style={{ textAlign: "center", marginTop: "3rem" }}>Launchers</h1>
       <LaunchList launchesData={launchesData} />
-    </div>
+    </>
   );
 };
